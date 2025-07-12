fc187f46d9e7c985e85e98663c312a9428f5401b
charles-sebastian-infante <charles_infante@dlsu.edu.ph>
2025-07-12 20:02:38 +0800

    finished addEdge implementation, added documentation

--- src/graph.c
+++ src/graph.c
@@ -3,14 +3,16 @@
 Node *
 createNode(char *name)
 {
-
+    // allocate memory to a new node
     Node *newNode = malloc(sizeof(Node));
 
+    // free the created node in event that newNode cannot be allocated memory
     if (newNode == NULL) {
+        free(newNode);
         printf("\nUnable to allocate memory for node.\n");
         return NULL;
     }
-
+    // set the identifier of the newNode, and set next to NULL
     newNode->name = name;
     newNode->next = NULL;
 
@@ -20,70 +22,101 @@ createNode(char *name)
 Graph *
 createGraph(int numVertices)
 {
+    // allocate memory for the graph and set # of vertices
     Graph *g = malloc(sizeof(Graph));
     g->numVertices = numVertices;
 
+    // allocate memory for the adjacency lists of g
     g->adjacencyList = malloc(numVertices * sizeof(Node *));
 
+    // free g in the event that adjacencyList cannot be allocated memory
     if (g->adjacencyList == NULL) {
+        free(g);
         printf("\nUnable to allocate memory for adjacency list.\n");
         return NULL;
     }
 
     for (int i = 0; i < numVertices; i++) {
         g->adjacencyList[i] = NULL;
+        g->adjacencyList[i]->nodeIndex = i;
     }
 
     return g;
 }
 
-// Call this for each line in the text file
-int
+
+bool
 createVertex(Graph *g, int vertex, char *name)
 {
-
+    // if vertex has been created
     if (g->adjacencyList[vertex] != NULL) {
         printf("\nThat vertex has already been created!\n");
-        return EXIT_FAILURE;
+        return false;
     }
 
     Node *newNode = createNode(name);
     g->adjacencyList[vertex] = newNode;
 
-    return EXIT_SUCCESS;
+    return true;
 }
 
-int
+bool
 hasEdge(Graph *g, int startVertex, char *name)
 {
     Node *current = g->adjacencyList[startVertex];
 
     while (current != NULL) {
         if (strcmp(name, current->name) == 0) {
-            return 1;
+            return true;
         }
         current = current->next;
     }
 
-    return 0;
+    return false;
 }
 
-int
-addEdge(Graph *g, int startVertex, char *name)
+bool
+addEdge(Graph *g, int startVertex, int endVertex, char *startName, char *endName)
 {
+    // initial check for duplicate edge
+    if (hasEdge(g, startVertex, endName) || hasEdge(g, endVertex, startName)){
+        return false;
+    }
+    
+    // create startVertex->endVertex connection
     Node *current = g->adjacencyList[startVertex];
     bool alreadyConnected = false;
 
     while (current != NULL && alreadyConnected) {
-        if (strcmp(current->name, name) != 0) {
+        if (strcmp(current->name, endName) != 0) {
             alreadyConnected = true;
         }
         current = current->next;
     }
 
     if (!alreadyConnected) {
-        current->next = createNode(name);
+        Node *new = createNode(endName);
+        new->next = g->adjacencyList[startVertex];
+        g->adjacencyList[startVertex] = new;
     }
+    
+    // create endVertex->startVertex connection
+    current = g->adjacencyList[endVertex];
+    alreadyConnected = false;
+    while (current != NULL && alreadyConnected){
+        if (strcmp(current->name, startName) != 0){
+            alreadyConnected = true;
+        }
+        current = current->next;
+    }
+
+    if (!alreadyConnected){
+        Node *new = createNode(startName);
+        new->next = g->adjacencyList[endVertex];
+        g->adjacencyList[endVertex] = new;
+    }
+
+    return true;
 }
 
 // temp and it's not in the format sir asked for
@@ -112,7 +145,7 @@ main(void)
 {
     Graph *test = createGraph(3);
     createVertex(test, 0, "Allen");
-    addEdge(test, 0, "Ryan");
+    addEdge(test, 0, 2, "Allen", "Ryan");
     createVertex(test, 1, "Jack");
     createVertex(test, 2, "Ryan");
 
--- src/graph.h
+++ src/graph.h
@@ -2,23 +2,84 @@
 #define GRAPH_HEADER
 
 /**
- * Programmed by: Allen Hizon
+ * Programmed by: Charles Infante
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-
+/**
+ * a Node structure for a doubly linked list implementation.
+ * @param name the node's unique identifier
+ * @param next pointer to the next node in the linked list
+ * @param nodeIndex the index of the node.
+ */
 typedef struct nodeTag {
     char *name;
     struct nodeTag *next;
+    int nodeIndex;
 } Node;
 
+/**
+ * an implementation of the Graph data structure
+ * @param numVertices is the number of vertices or Nodes in the graph
+ * @param adjacencyList 2D array of nodes
+ */
 typedef struct graphTag{
     int numVertices;
     Node** adjacencyList; 
        
 } Graph;
 
+/**
+ * creates a node with an identifier.
+ * @param name string of the unique identifier
+ * @return pointer to the created node if successful, NULL otherwise
+ */
+Node * createNode(char *name);
+/**
+ * creates a graph with a specified number of vertices/nodes.
+ * @param numVertices is the number of vertices in the graph.
+ * @return a pointer to the created graph if successful, NULL otherwise
+ */
+Graph * createGraph(int numVertices);
+/**
+ * creates a vertex in a specific graph with specific identifier.
+ * @param g is a pointer to the graph where vertex is added
+ * @param vertex is the index of the adjacency list to add to
+ * @param name is the identifier of the vertex
+ * @return true if vertex was successfully created, false otherwise
+ */
+bool createVertex(Graph *g, int vertex, char *name);
+/**
+ * checks if a graph has an edge from one vertex to another.
+ * @param g is a pointer to the graph where both vertices are located
+ * @param startVertex is the index of the adjacency list to add to
+ * @param name is the unique identifier of the terminal vertex to be checked
+ * @return true if an edge has been found, false otherwise
+ */
+bool hasEdge(Graph *g, int startVertex, char *name);
+/**
+ * adds an edge from one vertex to another
+ * @param g is a pointer to the graph to add a vertex.
+ * @param startVertex is the index of the start vertex's adjacency list
+ * @param endVertex is the index of the end vertex's adjacency list
+ * @param startName is the identifier of the start vertex
+ * @param endVertex is the identifier of the end vertex
+ * @return true if the edge is successfully created, false otherwise.
+ */
+bool addEdge(Graph *g, int startVertex, int endVertex, char *startName, char *endName);
+/**
+ * prints the adjacency list of a Node.
+ * @param head is the head node of a given adjacency list.
+ * @return void
+ */
+void printAdjacencyList(Node *head);
+/**
+ * prints a graph.
+ * @param g is the graph to be printed
+ * @return void
+ */
+void printGraph(Graph *g);
 #endif
