655de8362404f5dfdad12a43754c8bb6a99a24ab
allen <allen_hizon@dlsu.edu.ph>
2025-07-27 17:26:59 +0800

    moved dfs recursive function to separate file

--- src/output4-dfs.c
+++ src/output4-dfs.c
@@ -3,62 +3,23 @@
  * Tested by: Allen Hizon
  */
 #include "graph.h"
-#include <stdbool.h>
 #include <stdio.h>
 
-// Stack of vertices using an array
-typedef struct {
-    int top;
-    int vertices[MAX_VERTICES];
-} Stack;
-
-Stack *
-createStack()
-{
-    Stack *s = malloc(sizeof(Stack));
-    s->top = -1;
-    return s;
-}
-
-bool
-isStackFull(Stack *s)
-{
-    return s->top >= MAX_VERTICES - 1;
-}
-
-bool
-isStackEmpty(Stack *s)
-{
-    return s->top == -1;
-}
-
 void
-push(Stack *s, int value)
+recursiveDFS(FILE *fp, Graph *g, bool visited[], int vertex)
 {
-    if (isStackFull(s)) {
-        printf("The stack is full.");
-        return;
-    }
-    s->top++;
-    s->vertices[s->top] = value;
-}
 
-int
-pop(Stack *s)
-{
-    if (isStackEmpty(s)) {
-        printf("The stack is empty.");
-        return -1;
+    if (visited[vertex]) {
+        return; // immediately return to avoid revisiting
     }
 
-    int value = s->vertices[s->top];
-    s->top--;
-    return value;
-}
+    fprintf(fp, "%s ", g->adjacencyList[vertex]->name);
+    visited[vertex] = true;
 
-void
-printStack(Stack *s) {
-    for (int i = s->top; i > 0; i--) {
-        printf("[%d]\n", s->vertices[s->top]);
+    for (int i = 0; i < g->numVertices; i++) {
+        if (g->adjacencyMatrix[vertex][i] == true && !visited[i]) {
+            recursiveDFS(fp, g, visited, i);
+        }
     }
+    return;
 }
--- src/outputfiles.c
+++ src/outputfiles.c
@@ -193,26 +193,6 @@ outputBFS(char *strOutputFileName, Graph *g, char *vertex)
 
 
 void
-recursiveDFS(FILE* fp, Graph *g, bool visited[], int vertex)
-{
-
-    if (visited[vertex]) {
-        return; // immediately return to avoid revisiting
-    }
-
-    fprintf(fp, "%s ", g->adjacencyList[vertex]->name);
-    visited[vertex] = true;
-
-    for (int i = 0; i < g->numVertices; i++) {
-        if (g->adjacencyMatrix[vertex][i] == true && !visited[i]) {
-            recursiveDFS(fp, g, visited, i);
-        }
-    }
-    return;
-}
-
-
-void
 outputDFS(char *strOutputFileName, Graph *g, char *vertex)
 {
     FILE *fp = fopen(strOutputFileName, "w");
